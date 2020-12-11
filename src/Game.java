a4a427635cdb07c74a8405fbfdc8957f89f35e7d
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-11 10:33:10 +0000

    Moving to Map class

--- src/Game.java
+++ src/Game.java
@@ -58,37 +58,6 @@ public class Game
     }
 
     /**
-     * Create all the rooms and link their exits together.
-     */
-    private void createRooms()
-    {
-        Room outside, theater, pub, lab, office;
-
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-
-        // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
-
-        theater.setExit("west", outside);
-
-        pub.setExit("east", outside);
-
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
-
-        currentRoom = outside;  // start game outside
-    }
-
-    /**
      * Print out the opening message for the player.
      */
     private void printWelcome()
