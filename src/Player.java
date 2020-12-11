7a9688e78bcd7203db4f6ef9eec88d6e3987769f
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-11 13:23:23 +0000

    Added method to display the player's inventory

--- src/Player.java
+++ src/Player.java
@@ -30,4 +30,20 @@ public class Player
         score = 0;
         inventory = new ArrayList<>();
     }
+
+    /**
+     * Returns a string containing all items in the player's inventory.
+     * @return A string with all items currently in the player's possession.
+     */
+    public String getInventory()
+    {
+        String inventoryString = "";
+
+        for (Items item : inventory)
+        {
+            inventoryString += item.toString() + ", ";
+        }
+
+        return inventoryString;
+    }
 }
