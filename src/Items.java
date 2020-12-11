7743c2458553f4de530fcb32b3e01a469da663ca
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-11 11:08:11 +0000

    Added sample items

---
+++ src/Items.java
@@ -0,0 +1,11 @@
+/**
+ * Representations for all items in the game.
+ *
+ * @author Jason Huggins
+ * @version 11/12/2020
+ */
+public enum Items
+{
+    // readjust to fit with the game's context
+    NONE, WATER, BACKPACK, KEY, WATCH, COIN, NOTEPAD
+}
