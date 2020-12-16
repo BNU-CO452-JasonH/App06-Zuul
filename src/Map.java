80e834eaf824c05a622511bcd3cdeff8b127157a
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-16 22:45:41 +0000

    Added more rooms

--- src/Map.java
+++ src/Map.java
@@ -2,7 +2,7 @@
  * The Map class stores an interconnected set of rooms used for the Zuul game.
  *
  * @author Jason Huggins
- * @version 11/12/2020
+ * @version 16/12/2020
  */
 public class Map
 {
@@ -21,14 +21,17 @@ public class Map
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room outside, theater, pub, lab, office, library, field, shop;
 
         // create the rooms
-        outside = new Room(01, "outside", Items.BACKPACK);
-        theater = new Room(02, "theater", Items.COIN);
-        pub = new Room(03, "pub", Items.WATCH);
-        lab = new Room(04, "lab", Items.NOTEPAD);
-        office = new Room(05, "office", Items.WATER);
+        outside = new Room(1, "outside", Items.BACKPACK);
+        theater = new Room(2, "theater", Items.COIN);
+        pub = new Room(3, "pub", Items.WATCH);
+        lab = new Room(4, "lab", Items.NOTEPAD);
+        office = new Room(5, "office", Items.WATER);
+        library = new Room(6, "library", Items.NONE);
+        field = new Room(7, "field", Items.NONE);
+        shop = new Room(8, "shop", Items.KEY);
 
         // initialise room descriptions
         outside.setDescription("outside the main entrance of the university");
@@ -36,15 +39,26 @@ public class Map
         pub.setDescription("in the campus pub");
         lab.setDescription("in a computing lab");
         office.setDescription("in the computing admin office");
+        library.setDescription("in the library");
+        field.setDescription("on the university fields");
+        shop.setDescription("in the campus shop");
 
         // initialise room exits
+        outside.setExit("north", field);
         outside.setExit("east", theater);
         outside.setExit("south", lab);
         outside.setExit("west", pub);
 
+        field.setExit("south", outside);
+        field.setExit("east", shop);
+
         theater.setExit("west", outside);
 
         pub.setExit("east", outside);
+        pub.setExit("west", shop);
+
+        shop.setExit("west", field);
+        shop.setExit("east", pub);
 
         lab.setExit("north", outside);
         lab.setExit("east", office);
