5c284170b1ad8835a2d78b58fea4cbc349f60eb7
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-16 21:53:20 +0000

    Added methods to take and drop items

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * in possession.
  *
  * @author Jason Huggins
- * @version 11/12/2020
+ * @version 16/12/2020
  */
 public class Player
 {
@@ -18,7 +18,6 @@ public class Player
     private int energy;
     private int score;
     private ArrayList<Items> inventory;
-    private Room currentRoom;
 
     /**
      * Constructor for a Player object.
@@ -29,6 +28,30 @@ public class Player
         energy = MAX_ENERGY;
         score = 0;
         inventory = new ArrayList<>();
+    }
+
+    /**
+     * @return The player's currently set name.
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @return The player's current energy level.
+     */
+    public int getEnergy()
+    {
+        return energy;
+    }
+
+    /**
+     * @return The player's current score.
+     */
+    public int getScore()
+    {
+        return score;
     }
 
     /**
@@ -45,5 +68,58 @@ public class Player
         }
 
         return inventoryString;
+    }
+
+    /**
+     * Changes the player's current name to another name.
+     * @param name The new name for the player.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Changes the player's energy level to a given amount.
+     * @param energy The amount to set the player's energy level to.
+     */
+    public void setEnergy(int energy)
+    {
+        this.energy = energy;
+    }
+
+    /**
+     * Changes the player's score to a given amount.
+     * @param score The amount to set the player's score to.
+     */
+    public void setScore(int score)
+    {
+        this.score = score;
+    }
+
+    /**
+     * Picks up an item in the current room and stores it in the player's inventory.
+     * @param item The item to be picked up by the player.
+     */
+    public void takeItem(Items item)
+    {
+        inventory.add(item);
+    }
+
+    /**
+     * Drops an item stored in the player's inventory.
+     * @param item The item to be dropped by the player.
+     */
+    public void dropItem(Items item)
+    {
+        if (!inventory.contains(item))
+        {
+            System.out.println("Invalid choice: This item does not exist in your inventory.");
+        }
+        else
+        {
+            System.out.println("The following item has been dropped: " + item);
+            inventory.remove(item);
+        }
     }
 }
