4b071b85917c58ff706744e3dcf40eb6a1157c65
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-18 12:04:37 +0000

    Added todo

--- src/Items.java
+++ src/Items.java
@@ -2,10 +2,11 @@
  * Representations for all items in the game.
  *
  * @author Jason Huggins
- * @version 11/12/2020
+ * @version 18/12/2020
  */
 public enum Items
 {
+    // TODO: Add items that will increase the player's energy level and/or score.
     // The items present in the game.
     NONE, WATER, BACKPACK, KEY, WATCH, COIN, NOTEPAD
 }
