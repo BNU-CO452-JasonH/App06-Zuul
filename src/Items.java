39617ceba11d81ff9e724a7849f97deedd985f08
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2020-12-29 23:51:04 +0000

    Added a few more items

--- src/Items.java
+++ src/Items.java
@@ -2,11 +2,11 @@
  * Representations for all items in the game.
  *
  * @author Jason Huggins
- * @version 18/12/2020
+ * @version 29/12/2020
  */
 public enum Items
 {
     // TODO: Add items that will increase the player's energy level and/or score.
     // The items present in the game.
-    NONE, WATER, BACKPACK, KEY, WATCH, COIN, NOTEPAD
+    NONE, WATER, BACKPACK, KEY, WATCH, COIN, NOTEPAD, ENERGY_BAR, NECKLACE, COLA, BISCUIT, ID_CARD, PHONE
 }
